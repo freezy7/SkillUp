418a88c06bc38270c5ae9ae4148097d78531baf8
杜聪聪 <ducongcong@chelun.com>
2016-01-21 17:50:25 +0800

    add functionnal programming

---
+++ FunctionalProgramming/FunctionalProgramming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunctionalProgramming
+//
+//  Created by R_flava_Man on 16/1/21.
+//  Copyright © 2016年 R_style_Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
